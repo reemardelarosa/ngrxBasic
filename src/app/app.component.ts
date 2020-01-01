7c6fe98aa0edf3b4c8558d5bb79c80245722a17d
john.dela.rosa <jrddelarosa@gmail.com>
2020-01-01 10:48:07 +0800

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
+import { Store } from '@ngrx/store';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Post } from './models/post.model';
+import * as PostActions from './actions/post.actions';
 
+interface AppState {
+  post: Post;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ngrxFire1';
+  post: Observable<Post>;
+  text: string;
+
+  constructor(private store: Store<AppState>) {
+    this.post = this.store.select('post');
+  }
+
+  editText() {
+    this.store.dispatch(new PostActions.EditText(this.text));
+  }
+
+  resetPost() {
+    this.store.dispatch(new PostActions.Reset());
+  }
+
+  upvote() {
+    this.store.dispatch(new PostActions.Upvote());
+  }
+
+  downvote() {
+    this.store.dispatch(new PostActions.Downvote());
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreModule } from '@ngrx/store';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { postReducer } from './reducers/post.reducer';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    StoreModule.forRoot({
+      post: postReducer      
+    }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 10
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
